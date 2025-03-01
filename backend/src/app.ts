db064ef3d2e27ae1df1cfc2fefa6823d8d4e7e68
Dcoveney22 <156584156+Dcoveney22@users.noreply.github.com>
2025-03-01 08:09:04 +0000

    Starting Step 12

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,21 @@
 import express, { Request, Response } from 'express'
 import MemberAndGroupService from './services/MemberAndGroupService'
 import cors from 'cors'
+import AuthService from './services/AuthService'
+import 'dotenv/config'
+import ExpenseService from './services/ExpenseService'
+
+const jwtSecret = process.env.JWT_SECRET
+if (!jwtSecret) throw new Error('Missing ENV variables')
+
 const memberAndGroupService = new MemberAndGroupService()
+const authService = new AuthService(jwtSecret)
+const verifyToken = authService.verifyToken.bind(authService)
+const expenseService = new ExpenseService()
 
 const port = 3000
 const app = express()
+app.use(express.json())
 
 const options: cors.CorsOptions = {
   origin: ['http://localhost:5173'],
@@ -12,7 +23,15 @@ const options: cors.CorsOptions = {
 
 app.use(cors(options))
 
-app.get('/groups', async (req: Request, res: Response) => {
+app.get('/expenses/:groupID', verifyToken, async (req, res) => {
+  const groupID = Number(req.params.groupID)
+
+  const expenseCollection =
+    await expenseService.getExpenseCollectionForGroup(groupID)
+  res.status(200).json(expenseCollection)
+})
+
+app.get('/groups', verifyToken, async (req: Request, res: Response) => {
   const username = req.query.username
 
   const groups = await memberAndGroupService.getGroupsForUser(
@@ -20,6 +39,30 @@ app.get('/groups', async (req: Request, res: Response) => {
   )
 
   res.status(200).json(groups)
+})
+
+app.post('/expense', verifyToken, async (req, res) => {
+  const body = req.body
+
+  const { status, createdExpense } = await expenseService.addExpense(body)
+
+  res.status(status).json(createdExpense)
+})
+
+app.post('/authenticate', verifyToken, async (req: Request, res: Response) => {
+  const { username, password } = req.body
+
+  const { status, message } = await authService.checkCredentials(
+    username,
+    password
+  )
+
+  if (status === 200) {
+    const token = authService.generateToken(username)
+    res.status(status).json({ token, username })
+  } else {
+    res.status(status).json({ message })
+  }
 })
 
 app.listen(port, () => {
--- frontend/src/functions/getGroupsForMember.ts
+++ frontend/src/functions/getGroupsForMember.ts
@@ -1,8 +1,10 @@
-
-export async function getGroupsForMember(username: string) {
-    const response = await fetch(
-      `http://localhost:3000/groups?username=${username}`
-    )
-    if (!response.ok) throw new Error(response.statusText)
-    return await response.json()
-  }
+export async function getGroupsForMember(username: string, token: string) {
+  const response = await fetch(
+    `http://localhost:3000/groups?username=${username}`,
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  )
+  if (!response.ok) throw new Error(response.statusText)
+  return await response.json()
+}
